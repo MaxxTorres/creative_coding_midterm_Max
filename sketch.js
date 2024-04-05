f38fe3489587ecee94ca788b3d4a1a065cd9659f
MaxxTorres <157403152+MaxxTorres@users.noreply.github.com>
2024-04-05 18:46:08 -0400

    push 4 final revisions

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ let s1_angle;
 let s1_x;
 let s1_y;
 let s2_circles = [];
+let s2_shake;
 let timeline_x;
 let s2_back;
 let s2_forward;
@@ -31,14 +32,13 @@ function setup() {
   timeline_x = 0;
   s2_back = 10;
   s2_forward = 2.5;
+  s2_shake = false
   // SCENE3 //////
   s3_red_diff = 0;
   s3_red_diff_bg = 0;
 }
 
 function draw() {
-  print("ticks: " + ticks);
-  print("frame: " + frameCount);
   ticks++;
   if (scene == 1) {
     background(0);
@@ -47,7 +47,7 @@ function draw() {
     strokeWeight(1);
     fill(red);
     ellipse(red_x,red_y,red_size,red_size);
-    if (ticks >= 300) {
+    if (ticks >= 300) {   //red circle transition
       red_x += 20;
       red_y += 7;
     }
@@ -62,11 +62,22 @@ function draw() {
     createTimeLine();
     stroke(0);
     fill(255);
-    ellipse(100,400,80,100);
-    ellipse(100,350,50,50);
+    //Shake person once red circle is close
+    if (s2_shake) {
+      push();
+        translate(random(-2,2),random(-2,2));
+        ellipse(100,400,80,100);
+        ellipse(100,350,50,50);
+      pop();
+    } 
+    else {
+      ellipse(100,400,80,100);
+      ellipse(100,350,50,50);
+    }
     fill(red);
     ellipse(red_x,red_y,red_size,red_size);
     red_x -= s2_forward;
+    //Blue circle effects
     if (s2_circles.length < 15) {
       s2_circles.push(new Circle());
     }
@@ -77,6 +88,7 @@ function draw() {
         s2_circles.splice(i,1);
       }
     }
+    //Red circle gets pushed back, blue circles turn red
     if (red_x <= 210) {
       for (let i = 0; i < s2_circles.length; i++){
         (s2_circles[i]).turnRed = true;
@@ -84,28 +96,48 @@ function draw() {
       s2_back += 5;
       red_x += s2_back;
       s2_forward += 1;
+      s2_shake = true;
     }
     if(ticks >= 750) {
       scene = 3;
     }
   }
   else if (scene == 3) {
-    background(0+s3_red_diff_bg,0,0);
-    stroke(0);
-    fill(255,255-s3_red_diff,255-s3_red_diff);
-    ellipse(100,400,80,100);
-    ellipse(100,350,50,50);
-    noStroke();
-    fill(red);
-    ellipse(red_x,red_y,red_size,red_size);
     red_x -= 0.5;
     red_size += 1;
+    //Shake entire screen
     if (red_size >= 290) {
+      push();
+        translate(random(-5,5),random(-5,5));
+        background(0+s3_red_diff_bg,0,0);
+        stroke(0);
+        fill(255,255-s3_red_diff,255-s3_red_diff);
+        ellipse(100,400,80,100);
+        ellipse(100,350,50,50);
+        noStroke();
+        fill(red);
+        ellipse(red_x,red_y,red_size,red_size);
+      pop();
       s3_red_diff += 5;
     }
+    else {
+      background(0+s3_red_diff_bg,0,0);
+      stroke(0);
+      fill(255,255-s3_red_diff,255-s3_red_diff);
+      push();
+        translate(random(-5,5),random(-5,5));
+        ellipse(100,400,80,100);
+        ellipse(100,350,50,50);
+      pop();
+      noStroke();
+      fill(red);
+      ellipse(red_x,red_y,red_size,red_size);
+    }
+    //Background turns red
     if (red_size >= 350) {
       s3_red_diff_bg += 5;
     }
+    //"Restart" scenes
     if (s3_red_diff_bg >= 250) {
       background(0);
       reset();
@@ -114,6 +146,7 @@ function draw() {
 }
 
 function reset() {
+  frameRate(30);
   ticks = 0;
   red_size = 100;
   scene = 1;
@@ -126,6 +159,7 @@ function reset() {
   timeline_x = 0;
   s2_back = 10;
   s2_forward = 2.5;
+  s2_shake = false
   // SCENE3 //////
   s3_red_diff = 0;
   s3_red_diff_bg = 0;
@@ -159,7 +193,6 @@ function rotateLine() {
   }
 }
 /////////////////////////////////////////////////////
-
 
 // SCENE2 ///////////////////////////////////////////
 class Circle {
